322ddbf579e4505428b4d184130553c49cf1d007
spacester20 <86061567+spacester20@users.noreply.github.com>
2021-06-22 12:15:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char Str[1000], RevStr[100],  Str2[100];
+    int i,j,len,m,k,t,c=0;
+  
+    printf("Enter a word: ");
+    scanf("%s", Str);
+    
+    printf("\n Please Enter the Second word :  ");
+  scanf("%s", Str2);
+  
+    for (i = 0; Str[i] != '\0'; ++i);
+    
+    j = 0;
+  	len = i;
+ 
+  	for (m = i - 1; m >= 0; m--)
+  	{
+  		RevStr[j++] = Str[m];
+  	}
+  	RevStr[m] = '\0';
+  	
+  printf("Length is %d", i);
+  	printf("\n String after Reversing = %s", RevStr);
+  
+   if(Str2[i] == Str[i]){
+  	        printf("\nEqual\n");
+  	  }else{
+  	       printf("\nNot Equal\n");
+  	  }
+  	  
+  
+    return 0;
+}
